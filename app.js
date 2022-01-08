b0d1d482c56f348e1c4b84dc8c69ee748ff09a9d
Masayuki0927 <masayuki.0927.ikeda@gmail.com>
2022-01-08 14:57:39 +0900

    4th test for heroku

--- app.js
+++ app.js
@@ -619,7 +619,7 @@ app.get('/unfollow/:id', (req, res) => {
 //     })
 // })
 
-app.listen(3000);
+// app.listen(3000);
 
 server.listen(port, () => {
     console.log("App is running on port " + port);
